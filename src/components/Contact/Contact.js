cb250ee5d15f2455597fe480c0db06485e4c0e4d
Japhet Sebastian <japhet.sebastian01@gmail.com>
2021-06-03 19:10:49 +0300

    fix: horizontal form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,7 +7,7 @@ const Contact = () => (
         <div className="sm:overflow-hidden sm:rounded-md">
           <h2 className="block mt-6 mb-10 text-xl font-semibold text-gray-800">Interested? Reach me out</h2>
           <div className="py-5 bg-white space-y-8 sm:p-6">
-            <div className="col-span-6 sm:col-span-3">
+            <div className="col-span-6 sm:col-span-3 lg:inline-block">
               <label htmlFor="fullName" className="block text-base font-light text-gray-900">
                 Name
               </label>
@@ -18,7 +18,7 @@ const Contact = () => (
                 className="mt-4 p-2 focus:outline-none focus:ring focus:border-blue-300  block w-full shadow-sm sm:text-sm border-b-4 border-gray-500 rounded-md"
               />
             </div>
-            <div className="col-span-6 sm:col-span-4">
+            <div className="col-span-6 sm:col-span-4 lg:inline-block">
               <label htmlFor="emailAddress" className="block text-base font-light text-gray-900">
                 Email address
               </label>
