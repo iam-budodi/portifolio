5f704c983d9887726d7f3eb12240120fc62bb4c7
Japhet Sebastian <japhet.sebastian01@gmail.com>
2021-06-03 07:59:17 +0300

    fix: project page size

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -73,7 +73,7 @@ class CarouselSlider extends React.Component {
       );
     };
     return (
-      <div id="projects" className="my-2 space-y-12 rounded p-4 lg:p-12">
+      <div id="projects" className="my-2 space-y-12 rounded p-6">
         <div className="flex flex-col items-start">
           <p className="text-xl font-semibold leading-5 text-gray-800 lg:text-3xl">Some of my work</p>
         </div>
