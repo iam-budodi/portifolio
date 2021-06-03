b506c255fa73a757daf63bc6c00f4e4b0ba31c7b
Japhet Sebastian <japhet.sebastian01@gmail.com>
2021-06-03 18:18:46 +0300

    fix: projects size

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -73,11 +73,11 @@ class CarouselSlider extends React.Component {
       );
     };
     return (
-      <div id="projects" className="my-2 space-y-8 rounded p-6 lg:p-12">
+      <div id="projects" className="my-2 space-y-8 rounded p-6 lg:px-12 lg:py-8">
         <div className="">
           <p className="text-xl font-semibold leading-5 text-indigo-500 lg:text-3xl">Some of my work</p>
         </div>
-        <div className="lg:w-8/12 space-y-6 lg:mx-auto">
+        <div className="lg:w-8/12 space-y-4 lg:mx-auto">
           <div className="flex flex-col items-center">
             <p className="leading-5 text-gray-800 text-center">Explore my rich and diverse projects of different size and technologies</p>
           </div>
