37346381209a6315287582ec8badb62843e5cffb
Japhet Sebastian <japhet.sebastian01@gmail.com>
2021-06-03 22:16:50 +0300

    fix: centering the send button

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -46,7 +46,7 @@ const Contact = () => (
               <div className="lg:w-3/12 lg:mx-auto">
                 <button
                   type="submit"
-                  className="py-2 px-8 border border-transparent shadow-sm text-md font-medium rounded-md text-white bg-yellow-400 hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 w-3/12"
+                  className="py-2 px-8 border border-transparent shadow-sm text-md font-medium rounded-md text-white bg-yellow-400 hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 w-full"
                 >
                   Send
                 </button>
