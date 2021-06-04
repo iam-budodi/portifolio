17127afbbdd4026128430e07fc9403b317d87f16
Japhet Sebastian <japhet.sebastian01@gmail.com>
2021-06-04 19:52:12 +0300

    test: change header color for services

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -28,7 +28,7 @@ const Services = () => {
     <div id="services" className="p-4 lg:p-12 bg-gray-50">
       <div className="w-full shadow rounded border-t-2 px-6 py-4">
         <div className="flex items-end py-4 ">
-          <p className="text-xl font-semibold leading-5 text-indigo-300">Services I can offer</p>
+          <p className="text-xl font-semibold leading-5 text-indigo-500">Services I can offer</p>
         </div>
         {services.map(({
           faIcon, header, smallHeader, paragraph
@@ -45,9 +45,9 @@ const Services = () => {
               </div>
             </div>
             <div className="pl-3">
-              <p className="text-base font-semibold leading-normal text-indigo-500">{header}</p>
+              <p className="text-base font-semibold leading-normal text-indigo-300">{header}</p>
               <p className="text-sm leading-5 font-light text-indigo-200 pt-1">{smallHeader}</p>
-              <p className="pt-4 text-base leading-normal font-light text-gray-900">{paragraph}</p>
+              <p className="pt-4 text-base leading-normal font-light text-gray-700">{paragraph}</p>
             </div>
           </div>
         ))}
