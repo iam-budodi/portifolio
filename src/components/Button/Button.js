681b02158325637bf33a71937fbc1ae3d19d012a
Japhet Sebastian <japhet.sebastian01@gmail.com>
2021-06-05 20:30:13 +0300

    fixed: button transform on hover

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Button = () => (
-  <button type="button" className="transform motion-safe:hover:scale-90 bg-blue-800 py-2 px-4 rounded text-white">
+  <button type="button" className="transform motion-safe:hover:scale-90 hover:bg-red-600 bg-blue-800 py-2 px-4 rounded text-white">
     Visit this site
   </button>
 );
